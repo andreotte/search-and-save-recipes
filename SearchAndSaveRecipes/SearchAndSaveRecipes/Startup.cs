6318272b3f273ebbb5f9f093b0c4c248fbbfe717
Andre Otte <andre.b.otte1@gmail.com>
2019-06-06 11:24:46 -0400

    added solution folder

---
+++ SearchAndSaveRecipes/SearchAndSaveRecipes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchAndSaveRecipes.Startup))]
+namespace SearchAndSaveRecipes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
